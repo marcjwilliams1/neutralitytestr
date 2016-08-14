326fcbe3ee68589615e000094c3e35b226047a51
marcjwilliams1 <marc.williams.13@ucl.ac.uk>
2016-08-14 23:02:47 +0100

    added vignette

---
+++ R/TestingNeutrality.R
@@ -0,0 +1,12 @@
+load("/Users/marcwilliams/Google Drive/early_events/testingneutrality/data/testdata.RData")
+
+library(pracma);
+library(dplyr);
+library(ggplot2)
+library(scales)
+
+source("R/plots.R")
+source("R/multiplot.R")
+source("R/metrics.R")
+source("R/methods.R")
+source("R/processing.R")
