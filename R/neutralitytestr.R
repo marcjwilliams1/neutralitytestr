f147f638197cbcebebd00082ff078073037d4693
Marc <marc.williams.13@ucl.ac.uk>
2018-04-09 17:46:30 +0100

    Added documentation to package, no more warning notes in R CMD CHECK

---
+++ R/neutralitytestr.R
@@ -0,0 +1,13 @@
+#' \code{neutralitytestr} package
+#'
+#' Package to test a neutral evolutionary model on deep sequencing data.
+#'
+#' See the README on
+#' \href{https://github.com/marcjwilliams1/neutralitytestr#readme}{GitHub}
+#'
+#' @docType package
+#' @name neutralitytestr
+NULL
+
+utils::globalVariables(c("x", "lm", "data", "M_f", "inv_f",
+                         "VAF","..eq.label..","coefficients"))
